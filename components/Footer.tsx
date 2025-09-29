3daf60e44f3a1b21d45c96552a7589fd4c1b88e3
iamkiet <kietnguyen.me@gmail.com>
2025-09-30 01:09:01 +0700

    feat: fix dup layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="py-8 border-t border-gray-100 mt-16">
       <div className="text-center">
         <p className="text-xs text-gray-400 font-mono">
-          by kiet nguyen | coding | coffee | developer lifestyle | © 2025
+          by kiet nguyen | © 2025
         </p>
       </div>
     </footer>
